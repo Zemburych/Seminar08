0636a88eb493e52782d328c5d86f6a4d4fa560b4
Zemburych <116352585+Zemburych@users.noreply.github.com>
2022-11-10 14:57:38 +0300

    решили задачу 56

--- zad56/Program.cs
+++ zad56/Program.cs
@@ -1,4 +1,4 @@
-﻿// Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, 
+﻿// Задача 56: Задайте прямоугольный двумерный массив. Напишите программу,
 // которая будет находить строку с наименьшей суммой элементов.
 
 Console.Clear();
@@ -12,39 +12,49 @@ WriteArray(array);
 
 void CreateArray(int[,] array)
 {
-  for (int i = 0; i < array.GetLength(0); i++)
-  {
-    for (int j = 0; j < array.GetLength(1); j++)
+    for (int i = 0; i < array.GetLength(0); i++)
     {
-      array[i, j] = new Random().Next(1,9);
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 9);
+        }
     }
-  }
 }
-void WriteArray (int[,] array)
+void WriteArray(int[,] array)
 {
-  for (int i = 0; i < array.GetLength(0); i++)
-  {
-    for (int j = 0; j < array.GetLength(1); j++)
+    for (int i = 0; i < array.GetLength(0); i++)
     {
-      Console.Write(array[i,j] + " ");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
     }
-    Console.WriteLine();
-  }
 }
 
 int SummElemets(int[,] array, int i)
 {
-  int summ = array[i,0];
-  for (int j = 1; j < array.GetLength(1); j++)
-  {
-    summ = summ + array[i,j];
-  }
-  return summ;
-}
-int minsumm = 0;
-int summline = SummElemets(array,0);
-for (int i = 0; i < array.GetLength(0); i++);
+    int summ = array[i, 0];
+    for (int j = 1; j < array.GetLength(1); j++)
     {
-        if (SummElemets(array, i) > SummElemets(array, i + 1))
-        minsumm = SummElemets(array, i + 1);
+        summ = summ + array[i, j];
     }
+    return summ;
+}
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    Console.WriteLine($"Сумма строки {i} - " + SummElemets(array, i));
+}
+
+int minsumm = 0;
+int summLine = SummElemets(array, 0);
+for (int i = 1; i < array.GetLength(0); i++)
+{
+    int temp = SummElemets(array, i);
+    if (summLine > temp)
+    {
+        summLine = temp;
+        minsumm = (i);
+    }
+}
+Console.Write("Строка с наименьшей суммой - " + minsumm);
